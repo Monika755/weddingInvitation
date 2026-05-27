c828830351fc9bb954fdd517be32ec480a288412
Monika <monikabaghumyan0@gmail.com>
2026-05-27 10:14:37 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import "./App.css";
+
+export default function App() {
+  return (
+    <>
+      <div className="hero">
+        <h1 className="title">Հարսանյաց Հրավեր</h1>
+      </div>
+
+      <div className="invite">
+        <h2>Բարեկամներ և ընկերներ</h2>
+
+        <p>
+          Մենք գտել ենք մեր հավերժական սերը և ուրախ կլինենք, եթե կիսեք մեզ
+          հետ մեր լուսավոր օրը։
+        </p>
+
+        <h3>Սիրով հրավիրում ենք Ձեզ մեր</h3>
+
+        <h3>Հարսանիքին</h3>
+      </div>
+    </>
+  );
+}
